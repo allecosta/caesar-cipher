c1cf6b5395ca9c349656cbee9d99b50b499aaa2f
allecosta <alle_costa10@outlook.com>
2025-01-23 17:42:53 -0300

    refactor: some changes after debbug
    Signed-off-by: allecosta <alle_costa10@outlook.com>

--- src/caesar.js
+++ src/caesar.js
@@ -1,6 +1,6 @@
-const FREQUENCY = "AEOSRNIDMUCTLBPVGHFQJXZYWK".split("");
+const FREQUENCY = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
-// Função para calcular a frequência das letras no texto
+// Calcula a frequencia das letras no texto
 function calcFrequency(text) {
     let cont = {};
 
@@ -12,26 +12,26 @@ function calcFrequency(text) {
     return Object.entries(cont).sort((a, b) => b[1] - a[1]);
 }
 
-// Função para tentar descobrir a chave por análise de frequência
+// Verifica a letra mais frequente no texto cifrado e tenta calcular o possivel deslocamento
 function discoverKey(textEncrypted){
-    const frequencyText = calcFrequency(textEncrypted);
+    const FREQUENCY_TEXT = calcFrequency(textEncrypted);
 
-    if (frequencyText.length === 0) {
+    if (FREQUENCY_TEXT.length === 0) {
         console.log("OPS! Não encontramos nenhuma letra válida no texto.");
         return null;
     }
 
-    let frequentLetter = frequencyText[0][0];
-    let possibleKey= [];
+    let frequencyLetter = FREQUENCY_TEXT[0][0];
+    let possibleKeys= [];
 
     for (let referenceLetter of FREQUENCY) {
-        let displacement = (frequentLetter.charCodeAt(0) - referenceLetter.charCodeAt(0) + 26) % 26;
-        possibleKey.push(displacement);
+        let displacement = (frequencyLetter.charCodeAt(0) - referenceLetter.charCodeAt(0) + 26) % 26;
+        possibleKeys.push(displacement);
     }
-    return possibleKey;
+    return possibleKeys;
 }
 
-// Função para decifrar um texto usando uma chave de deslocamento
+// Tenta decifrar o texto usando a chave identificada
 function decodeCaesar(text, displacement) {
     return text
         .split("")
@@ -49,11 +49,10 @@ function decodeCaesar(text, displacement) {
 * A B C D E F G H I J  K  L  M  N  O  P  Q  R  S  T  U  V  W  X  Y  Z
 * 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
 * */
-const textEncrypted = "VHJXUDQFD FLEHUQHWLFD";
 
-const keyPossible = discoverKey(textEncrypted);
-console.log("Possíveis chaves:", keyPossible);
-
-keyPossible.forEach(key => {
-    console.log(`Tentativa com chave ${key}:`, decodeCaesar(textEncrypted, key));
+let textEncrypted = "VHJXUDQFD FLEHUQHWLFD";
+let possibleKey = discoverKey(textEncrypted);
+console.log("Chaves: ", possibleKey);
+possibleKey.forEach(key => {
+    console.log(`Chave ${key}:`, decodeCaesar(textEncrypted, key));
 });
